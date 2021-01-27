b6b5f52f40378e4385d0ee4a2395f3782e10a691
Kyle Rose <krosey1212@gmail.com>
2021-01-26 19:33:50 -0500

    init

---
+++ config/teams.py
@@ -0,0 +1,183 @@
+
+playoff_teams = {
+    "2019": ["rav", "kan", "nwe", "htx", "buf", "oti", "sfo", "gnb", "nor", "phi", "sea", "min"],
+    "2020": ["buf", "kan", "rav", "oti", "clt", "pit", "cle", "chi", "gnb", "nor", "tam", "sea", "ram", "was"]
+}
+
+teams = {
+    # AFC East
+    "buf": {
+        "teamFull": "Buffalo Bills",
+        "mascot": "Bills",
+        "teamShort": "BUF"
+    },
+    "nwe": {
+        "teamFull": "New England Patriots",
+        "mascot": "Patriots",
+        "teamShort": "NE"
+    },
+    "mia": {
+        "teamFull": "Miami Dolphins",
+        "mascot": "Dolphins",
+        "teamShort": "MIA"
+    },
+    "nyj": {
+        "teamFull": "New York Jets",
+        "mascot": "Jets",
+        "teamShort": "NYJ"
+    },
+
+    # AFC North
+    "pit": {
+        "teamFull": "Pittsburg Steelers",
+        "mascot": "Steelers",
+        "teamShort": "PIT"
+    },
+    "rav": {
+        "teamFull": "Baltimore Ravens",
+        "mascot": "Ravens",
+        "teamShort": "BAL"
+    },
+    "cle": {
+        "teamFull": "Cleveland Browns",
+        "mascot": "Browns",
+        "teamShort": "CLE"
+    },
+    "cin": {
+        "teamFull": "Cincinnati Bengals",
+        "mascot": "Bengals",
+        "teamShort": "CIN"
+    },
+
+    # AFC South
+    "oti": {
+        "teamFull": "Tennessee Titans",
+        "mascot": "Titans",
+        "teamShort": "TEN"
+    },
+    "clt": {
+        "teamFull": "Indianapolis Colts",
+        "mascot": "Colts",
+        "teamShort": "IND"
+    },
+    "htn": {
+        "teamFull": "Houston Texans",
+        "mascot": "Texans",
+        "teamShort": "HOU"
+    },
+    "jax": {
+        "teamFull": "Jacksonville Jaguars",
+        "mascot": "Jaguars",
+        "teamShort": "JAX"
+    },
+
+    # AFC West
+    "kan": {
+        "teamFull": "Kansas City Chiefs",
+        "mascot": "Chiefs",
+        "teamShort": "KC"
+    },
+    "rai": {
+        "teamFull": "Las Vegas Raiders",
+        "mascot": "Raiders",
+        "teamShort": "LV"
+    },
+    "sdg": {
+        "teamFull": "Los Angeles Chargers",
+        "mascot": "Chargers",
+        "teamShort": "LAC"
+    },
+    "den": {
+        "teamFull": "Denver Broncos",
+        "mascot": "Broncos",
+        "teamShort": "DEN"
+    },
+
+    # NFC East
+    "was": {
+        "teamFull": "Washington Football Team",
+        "mascot": "Football Team",
+        "teamShort": "WAS"
+    },
+    "nyg": {
+        "teamFull": "New York Giants",
+        "mascot": "Giants",
+        "teamShort": "NYG"
+    },
+    "dal": {
+        "teamFull": "Dallas Cowboys",
+        "mascot": "Cowboys",
+        "teamShort": "DAL"
+    },
+    "phi": {
+        "teamFull": "Philadelphia Eagles",
+        "mascot": "Eagles",
+        "teamShort": "PHI"
+    },
+
+    # NFC North
+    "gnb": {
+        "teamFull": "Green Bay Packers",
+        "mascot": "Packers",
+        "teamShort": "GB"
+    },
+    "chi": {
+        "teamFull": "Chicago Bears",
+        "mascot": "Bears",
+        "teamShort": "CHI"
+    },
+    "min": {
+        "teamFull": "Minnesota Vikings",
+        "mascot": "Vikings",
+        "teamShort": "MIN"
+    },
+    "det": {
+        "teamFull": "Detroit Lions",
+        "mascot": "Lions",
+        "teamShort": "DET"
+    },
+
+    # NFC South
+    "nor": {
+        "teamFull": "New Orleans Saints",
+        "mascot": "Saints",
+        "teamShort": "NO"
+    },
+    "tam": {
+        "teamFull": "Tampa Bay Bucs",
+        "mascot": "Bucs",
+        "teamShort": "TB"
+    },
+    "car": {
+        "teamFull": "Carolina Panthers",
+        "mascot": "Panthers",
+        "teamShort": "CAR"
+    },
+    "atl": {
+        "teamFull": "Atlanta Falcons",
+        "mascot": "Falcons",
+        "teamShort": "ATL"
+    },
+
+    # NFC West
+    "sea": {
+        "teamFull": "Seattle Seahawks",
+        "mascot": "Seahawks",
+        "teamShort": "SEA"
+    },
+    "ram": {
+        "teamFull": "Los Angeles Rams",
+        "mascot": "Rams",
+        "teamShort": "LAR"
+    },
+    "sfo": {
+        "teamFull": "San Francisco 49ers",
+        "mascot": "49ers",
+        "teamShort": "SF"
+    },
+    "crd": {
+        "teamFull": "Arizona Cardinals",
+        "mascot": "Cardinals",
+        "teamShort": "ARI"
+    }
+}
--- rosters.py
+++ rosters.py
@@ -1,1 +1,37 @@
-print("Hello World")
+import requests
+import pandas as pd
+
+from .config.teams import teams, playoff_teams
+
+_YEAR = '2020'
+
+def main(year, output_file=False):
+    current_year_playoff_teams = playoff_teams[year]
+    playoff_team_attrs = {team: attrs for team, attrs in teams.items() if team in current_year_playoff_teams}
+
+    rosters = []
+    for key, val in playoff_team_attrs.items():
+        url = f'https://www.pro-football-reference.com/teams/{key}/{_YEAR}_roster.htm'
+        print(url)
+        page = requests.get(url)
+        df = pd.read_html(page.text.replace('<!--',''))[1]
+        df = df[df['Pos'].isin(['WR','TE','QB','RB','FB','K','RB/WR'])][['Player','Pos','G']]
+        df['Team'] = val['teamFull']
+        df['TeamMascot'] = df['Team'].apply(lambda x: x.split(' ')[-1])
+        df['TeamShort'] = val['teamShort']
+        df['TeamKey'] = key.upper()
+        rosters.append(df)
+        
+    df_combined = pd.concat(rosters)
+    df_combined.columns = [c.lower() for c in df_combined.columns]
+
+    if output_file:
+        df_combined.to_csv(f'datasets/rosters{_YEAR}.csv', index = False)
+        print("Output File Successfully Created!")
+
+    return df_combined
+
+
+if __name__ == "__main__":
+    df = main(_YEAR, output_file=True)
+    print(df)
